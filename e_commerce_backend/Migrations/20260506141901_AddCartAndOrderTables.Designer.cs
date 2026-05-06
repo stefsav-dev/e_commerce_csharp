871ca5e58f4f62d06b24f645cb92d9185dc5e48a
Stefanus Andre <stefanusandre07@gmail.com>
2026-05-06 23:01:10 +0700

    update migrations

--- e_commerce_backend/Migrations/20260506141901_AddCartAndOrderTables.Designer.cs
+++ e_commerce_backend/Migrations/20260506141901_AddCartAndOrderTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e_commerce_backend.Data;
 
@@ -11,9 +12,11 @@ using e_commerce_backend.Data;
 namespace e_commerce_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260506141901_AddCartAndOrderTables")]
+    partial class AddCartAndOrderTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
